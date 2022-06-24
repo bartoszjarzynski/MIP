551f12aa02006d34d99b7b87a69182d8ca1909f8
Bartosz Jarzyński <72377404+bartoszjarzynski@users.noreply.github.com>
2022-06-24 12:43:39 +0200

    🎨 Finished project
    MIP (Maximum Intensity Projection) is a Python app which lets you display a 3D data of visualization the voxels from different views. It is created by using MIP images.

--- generator.py
+++ generator.py
@@ -7,39 +7,39 @@ import glob
 from PIL import Image, ImageTk
 import os
 import numpy as np
-import pandas as pd
+import SimpleITK as sitk
+import fredtools as ft
 
 # Object class
-class obraz:
+class Picture:
     # Initializing object
     def __init__(s):
         s.pix = []
         s.zdj = ImageTk
         s.canvas = Canvas(root)
         s.click = 1
+        s.matrix = []
 
     # Rotate an image
-    def fun(s, pix):
+    def rotate(s, pix):
         s.canvas.destroy()
         if len(pix) > 0:
             s.pix = np.rot90(pix, 1, (1,0))
             s.pix = np.rot90(s.pix, s.click, (2,0))
-            szerokosc = 1024
-            wysokosc = 512
-            s.canvas = Canvas(root, width=szerokosc, height=wysokosc)
+            width_ = 1024
+            height_ = 512
+            s.canvas = Canvas(root, width=width_, height=height_)
             s.canvas.pack(side='right')
-            s.rys()
-            #print(s.pix)           ### PRINT ALL THE PIXELS VALUES
-            #print(s.pix[0][0])     ### PRINT FIRST PIXEL VALUE
+            s.draw()
 
     # Drawing an image
-    def rys(s):
+    def draw(s):
         s.canvas.delete('all')
-        s.zdj = ImageTk.PhotoImage(image=Image.fromarray(s.pix[1]))
+        s.zdj = ImageTk.PhotoImage(image=Image.fromarray(s.pix[100]))
         s.canvas.create_image(0, 0, anchor="nw", image=s.zdj)
 
     # Reading a path with image
-    def wczytaj(s, path):
+    def load(s, path):
         pix = []
         if os.path.isdir(path):
             for file in glob.glob(path + '/*.dcm'):
@@ -48,63 +48,49 @@ class obraz:
         return pix
 
     # Rotating by 1
-    def obrot(s):
+    def roll(s):
         s.click += 1
         
     def get_highest_value(s):
-        #s.pix[picture_number]
-        #s.pix[0][0] - first picture
-        #s.pic[0][0][0] - first pixel
+        #s.pic[511][199][511] - last picture and pixel   
+        s.matrix = s.pix[0]
         
-        for i in range (0, len(s.pix)-1):
-            for j in range (0, len(s.pix[i])):
-                for k in range (0, len(s.pix[i][j])):
-                    
-                    if s.pix[i+1][j][k] > s.pix[i][j][k]:
-                        s.pix[i][j][k] = s.pix[i+1][j][k]
-            
-        print(s.pix[0][0])
-        
-    # Getting the highest value in an array
-    # def get_highest_value(s):
-        
-    #     all_arrays = np.array_split(s.pix, 512)
-    #     new_image_arr = []
-        
-    #     for i in range(len(all_arrays)):
-    #         try:
-    #             highestarrays = np.max(all_arrays[i])
-    #             # highestarrays = int(highestarrays)
-    #         except ValueError: # empty sequence
-    #             pass
+        for i in range(0, 511):
+            for j in range(0, len(s.pix[i])):
+                for k in range(0, len(s.pix[i][j])):
 
-    #     new_image_arr.append(highestarrays)
-    #     print(new_image_arr)
+                    if s.pix[i][j][k] > s.matrix[j][k]:
+                        s.matrix[j][k] = s.pix[i][j][k]
+        print(s.matrix)    
+
 
 root = Tk()
-root.state('zoomed')
+# The view of the window
+root.state('normal')
 root.title('MIP')
 
-pole = obraz()
+pole = Picture()
 
 menu = Frame(root)
-menu.pack(side='left', padx=20, pady=20)
+menu.pack(side='top', padx=30, pady=30)
 
-title = Label(menu, text='MIP', font=('Arial', 20))
+title = Label(menu, text='MIP', font=('Arial', 25))
 title.pack()
 
 # Button for opening a file
-button1 = Button(menu, text='Choose a directory', command=lambda: pole.fun(pole.wczytaj(fd.askdirectory())))
+button1= Button(menu, text='Choose a directory', command=lambda: pole.rotate(pole.load(fd.askdirectory())))
 button1.pack()
 # Button for rotating by 1
-button2 = Button(menu, text='Rotate', command=lambda: pole.obrot())
+button2 = Button(menu, text='Rotate', command=lambda: pole.roll())
 button2.pack()
 # Button for highest value
 button3 = Button(menu, text='Max value', command=lambda: pole.get_highest_value())
 button3.pack()
 
-button4 = Button(menu, text='Display new image *TO DO*', command=lambda: pole.fun(pole.wczytaj(fd.askdirectory())))
+button4 = Button(menu, text='Exit', command=lambda: exit())
 button4.pack()
+
+# TO DO: displaying new, the  brightest image button
 
 # Looping the window
 root.mainloop()
